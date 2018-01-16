ace27c8f93ab6512d8629c56892fc205131102ff
yinweli <yinweli@yahoo.com.tw>
2018-01-16 13:25:38 +0800

    新增json配接器

---
+++ Assets/Addon/FouridStudio/Adaper/JsonAdaper.cs
@@ -0,0 +1,23 @@
+﻿namespace FouridStudio
+{
+    /// <summary>
+    /// json配接器
+    /// </summary>
+    /// <typeparam name="T">資料型態</typeparam>
+    public interface JsonAdaper<T>
+    {
+        /// <summary>
+        /// 從物件轉為json字串
+        /// </summary>
+        /// <param name="obj">物件</param>
+        /// <returns>json字串</returns>
+        string toJson(T obj);
+
+        /// <summary>
+        /// 從json字串轉為物件
+        /// </summary>
+        /// <param name="json">json字串</param>
+        /// <returns>物件</returns>
+        T toObject(string json);
+    }
+}
