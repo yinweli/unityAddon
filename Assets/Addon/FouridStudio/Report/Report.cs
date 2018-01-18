01ea0750cdf1f4b7e47aa54157e237a1b146af82
yinweli <yinweli@yahoo.com.tw>
2018-01-18 12:01:44 +0800

    更新回報組件

--- Assets/Addon/FouridStudio/Report/Report.cs
+++ Assets/Addon/FouridStudio/Report/Report.cs
@@ -1,51 +1,91 @@
-﻿using UnityEngine;
+﻿using System;
+using System.IO;
+using UnityEngine;
 
 namespace FouridStudio
 {
     /// <summary>
-    /// UnityEngine.Debug報告
+    /// 回報執行紀錄
     /// </summary>
     public class Report : Singleton<Report>
     {
         #region 主要函式
 
         /// <summary>
-        /// 紀錄
+        /// 回報訊息
         /// </summary>
-        /// <param name="message">訊息</param>
-        public void log(string message)
+        /// <param name="message">訊息字串</param>
+        public void info(string message)
         {
-            Debug.Log("[Log]" + message);
+            outputLog("[Info] " + message);
         }
 
         /// <summary>
-        /// 訊息
+        /// 回報錯誤
         /// </summary>
-        /// <typeparam name="T">傳回值型態</typeparam>
-        /// <param name="message">訊息</param>
-        /// <param name="obj">傳回物件</param>
-        /// <returns>傳回物件</returns>
-        public T info<T>(string message, T obj)
+        /// <param name="message">訊息字串</param>
+        public void error(string message)
         {
-            Debug.Log("[Info] " + message);
-
-            return obj;
+            outputLog("[Error] " + message);
         }
 
         /// <summary>
-        /// 錯誤
+        /// 回報錯誤
         /// </summary>
-        /// <typeparam name="T">傳回值型態</typeparam>
-        /// <param name="message">訊息</param>
-        /// <param name="obj">傳回物件</param>
-        /// <returns>傳回物件</returns>
-        public T error<T>(string message, T obj)
+        /// <param name="message">訊息字串</param>
+        /// <param name="exception">錯誤物件</param>
+        public void error(string message, Exception exception)
         {
-            Debug.Log("[Error] " + message);
+            outputLog("[Error] " + message);
+            outputLog("[Exception] " + exception);
+        }
 
-            return obj;
+        /// <summary>
+        /// 回報錯誤
+        /// </summary>
+        /// <param name="exception">錯誤物件</param>
+        public void error(Exception exception)
+        {
+            outputLog("[Exception] " + exception);
         }
 
         #endregion 主要函式
+
+        #region 內部函式
+
+        private void outputLog(string message)
+        {
+            outputDebugLog(message);
+            outputReleaseLog(message);
+        }
+
+        private void outputDebugLog(string message)
+        {
+            if (Debug.isDebugBuild)
+                Debug.Log(message);
+        }
+
+        private void outputReleaseLog(string message)
+        {
+            Directory.CreateDirectory(GamePath.Instance.PathLog);
+
+            string logFileName = getCurrentDateString() + " " + Application.productName + ".log";
+            string logFilePath = Path.Combine(GamePath.Instance.PathLog, logFileName);
+            string logMessageWithTime = getCurrentTimeString() + " " + message + Environment.NewLine;
+
+            File.AppendAllText(logFilePath, logMessageWithTime);
+        }
+
+        private string getCurrentDateString()
+        {
+            return DateTime.Now.ToString("yyyy-MM-dd");
+        }
+
+        private string getCurrentTimeString()
+        {
+            return DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+        }
+
+        #endregion 內部函式
     }
 }
