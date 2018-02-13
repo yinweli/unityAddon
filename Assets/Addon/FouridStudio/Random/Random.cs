18691fde54d18d1b9cfbb62af03c2adb3dce22ff
yinweli <yinweli@yahoo.com.tw>
2018-02-13 15:05:14 +0800

    新增隨機組件

---
+++ Assets/Addon/FouridStudio/Random/Random.cs
@@ -0,0 +1,72 @@
+﻿namespace FouridStudio
+{
+    /// <summary>
+    /// 預設隨機類別
+    /// </summary>
+    public class Random : RandomInterface
+    {
+        #region 屬性
+
+        /// <summary>
+        /// 使用系統提供的隨機物件
+        /// </summary>
+        private System.Random random = null;
+
+        /// <summary>
+        /// 隨機種子
+        /// </summary>
+        private string seed = "default seed";
+
+        #endregion 屬性
+
+        #region 主要函式
+
+        public Random()
+        {
+            this.random = new System.Random();
+        }
+
+        public Random(int seed)
+        {
+            this.random = new System.Random(seed);
+            this.seed = seed.ToString();
+        }
+
+        public string getSeed()
+        {
+            return seed;
+        }
+
+        /// <summary>
+        /// 取得隨機值
+        /// </summary>
+        /// <returns>隨機值</returns>
+        public int randomValue()
+        {
+            return random.Next();
+        }
+
+        /// <summary>
+        /// 取得隨機值
+        /// </summary>
+        /// <param name="max">最大值</param>
+        /// <returns>隨機值</returns>
+        public int randomValue(int max)
+        {
+            return random.Next(max);
+        }
+
+        /// <summary>
+        /// 取得隨機值
+        /// </summary>
+        /// <param name="min">最小值</param>
+        /// <param name="max">最大值</param>
+        /// <returns>隨機值</returns>
+        public int randomValue(int min, int max)
+        {
+            return random.Next(min, max);
+        }
+
+        #endregion 主要函式
+    }
+}
---
+++ Assets/Addon/FouridStudio/Random/RandomInterface.cs
@@ -0,0 +1,16 @@
+﻿namespace FouridStudio
+{
+    /// <summary>
+    /// 隨機取值介面
+    /// </summary>
+    public interface RandomInterface
+    {
+        string getSeed();
+
+        int randomValue();
+
+        int randomValue(int max);
+
+        int randomValue(int min, int max);
+    }
+}
